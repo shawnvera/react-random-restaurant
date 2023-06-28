897991334297df3d94ef7050e70c279bc64d0dbe
Shawn Vera <shawnvera@gmail.com>
2023-06-28 23:34:49 +0000

    added react JS components

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import NavBar from './NavBar.js';
+import Menu from './Menu.js';
+import Header from './Header.js'
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  
+    <Header />
   </React.StrictMode>
 );
