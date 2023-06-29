b265d16eadfde7ba1c820e70d68b2e880cd871c5
Shawn Vera <shawnvera@gmail.com>
2023-06-29 16:06:59 +0000

    added api call

--- app/src/Menu.js
+++ app/src/Menu.js
@@ -2,8 +2,19 @@ import React from "react";
 
 export default function Menu () {
     return (
+<>
+        <div className="card">
+                <img height="100" width="125" src="https://cdn.pixabay.com/photo/2017/11/11/20/19/dinner-table-2940339_1280.png" className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+            </div>
+
 <li>
     Hello!
 </li>
+</>
     )
 }
--- app/src/NavBar.js
+++ app/src/NavBar.js
@@ -5,14 +5,14 @@ export default function NavBar() {
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <a className="navbar-brand" href="./index.js">Lex.Eat.with(friends)</a>
+                    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
-                    </button>
+                    </button> */}
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <a className="nav-link active" aria-current="page" href="./index.js">Home</a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="#">Lunch</a>
@@ -21,7 +21,7 @@ export default function NavBar() {
                                 <a className="nav-link" href="#">Dinner</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled">Disabled</a>
+                                <a className="nav-link disabled">About Us</a>
                             </li>
                         </ul>
                     </div>
